142aad3f8c4a9eea6452a835f40c9d303d2258a8
Michael <schmiddim@gmx.at>
2023-09-11 09:05:18 +0000

    OK

--- run.sh
+++ run.sh
@@ -1,6 +1,9 @@
+#!/bin/sh
+docker build --tag=unit-webapp .
 export UNIT=$(                                                         \
-      docker run                                                        \
+      docker run -d                                                      \
       --mount type=bind,src="$(pwd)/config/",dst=/docker-entrypoint.d/   \
+      --mount type=bind,src="$(pwd)/log/unit.log",dst=/var/log/unit.log  \
       --mount type=bind,src="$(pwd)/state",dst=/var/lib/unit             \
       --mount type=bind,src="$(pwd)/webapp",dst=/www                     \
       -p 8080:8000 unit-webapp                                           \
